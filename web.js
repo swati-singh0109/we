2963cb35e2083c7826f7055dc0af131efd173c44
swati-singh0109 <swatijhts@gmail.com>
2024-12-28 15:12:31 +0530

    Add files via upload

---
+++ web.js
@@ -0,0 +1,5 @@
+document.getElementById('languageSelector').addEventListener('change', function() {
+    let language = this.value;
+    alert('Language changed to: ' + language);
+    // Implement actual language change functionality here
+});
